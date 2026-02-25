976f2bbec3c1b4a59d67e8c2c7eb031ef68114ea
Tran Ngoc Son <son.tran@sweetsoft.vn>
2026-02-25 09:56:52 +0700

    test connect db

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/mysql2'
 import mysql from 'mysql2/promise'
 import * as schema from '../models/user.model'
 
-const connection = await mysql.createConnection({
+const connection = await mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER || 'root',
